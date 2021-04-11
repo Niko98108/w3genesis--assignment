1296caa2610fb9a1fa040e0bedaffd383d87ebec
Nisal <iamnisal123@gmail.com>
2021-04-11 12:38:26 +0530

    copy link fuction mistake correction

--- app/Views/organizerDashboard/dashboard.php
+++ app/Views/organizerDashboard/dashboard.php
@@ -53,7 +53,7 @@
                                                 </p>
                                                 <p class="card-text">
                                                 <a href =""  data-toggle="modal" data-target="#exampleModalCenter"><button class="btn btn-success  mb-4 mr-2 btn-sm" >Share</button></a>
-                                                <a class="nav-link" href="" data-toggle="modal" data-target="#exampleModalCenter" >Share</a>
+                                           
                                                 </p>
                                         </div>
                                     </div>
@@ -65,8 +65,6 @@
                                         <div class="counter-content">
                                                      <h1 class="display-4"><?=$users;?></h1>
                                                 </div>
-                                                    <p class="ico-counter-text">Users</p>
-                                          
                                                 <h5 class="card-title">Register Users</h5>
                                                 <p class="card-text">
                                                 <a href ="<?=base_url();?>/OrgernizerDashboard/Users"><button class="btn btn-info  mb-4 mr-2 btn-sm" >View</button></a>
@@ -80,8 +78,6 @@
                                         <div class="counter-content">
                                                      <h1 class="display-4"><?=$events;?></h1>
                                                 </div>
-                                                    <p class="ico-counter-text">Events</p>
-                                         
                                                 <h5 class="card-title">All Events</h5>
                                                 <p class="card-text">
                                                 <a href ="<?=base_url();?>/OrgernizerDashboard/events"><button class="btn btn-info  mb-4 mr-2 btn-sm" >View</button></a>
@@ -101,52 +97,65 @@
                 </div>
             </div>
 </div>       
-<!-- Modal -->
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" style="display: none;" aria-hidden="true">
-                                        <div class="modal-dialog modal-dialog-centered" role="document">
-                                            <div class="modal-content">
-                                                <div class="modal-header">
-                                                    <h5 class="modal-title" id="exampleModalCenterTitle">Share Register Form</h5>
-                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
-                                                </div>
-                                                <div class="modal-body">
-                                                    <div class= "row">
-                                                        <div class="col">
-                                                            <a href="https://www.facebook.com/sharer/sharer.php?u="target="_blank"> <img src="<?=base_url();?>/assets/img/social/facebook.png" alt="facebook" style="width:60px;"></a>
-                                                        </div>
-                                                        <div class="col">
-                                                          <a href="#" target="_blank"> <img src="<?=base_url();?>/assets/img/social/instagram.png" alt="facebook" style="width:60px;"></a>
-                                                        </div>
-                                                        <div class="col">
-                                                            <a href="https://www.linkedin.com/shareArticle?mini=true&url=" target="_blank"> <img src="<?=base_url();?>/assets/img/social/linkedin.png" alt="facebook" style="width:60px;"></a>
-                                                        </div>
-                                                        <div class="col">
-                                                            <a href="https://twitter.com/share?url=&text=&hashtags=" target="_blank"> <img src="<?=base_url();?>/assets/img/social/twitter.png" alt="facebook" style="width:60px;"></a>
-                                                        </div>
-                                                        <div class="col">
-                                                            <a href="https://api.whatsapp.com/send?text=&url="target="_blank"> <img src="<?=base_url();?>/assets/img/social/whatsapp.png" alt="facebook" style="width:60px;"></a>
-                                                        </div>
-                                                        <div class="col">
-                                                            <a href=""> <img src="<?=base_url();?>/assets/img/social/youtube.png" alt="facebook" style="width:60px;"></a>
-                                                        </div>
-                                                        <br>
-                                                        <br>
-                                                        <hr>
-                                                      <div class ="form-row">
-                                                    </div>
-                                                       <div class="form-group col-sm-12">
-                                                         <label for="copylink">copy Link</label>
-                                                            <input type="text" name="link" value="" class="form-control" id="" readonly="readonly">
-                                                        </div>
-                                                     </div>
-                                                        <div>
-                                                        <button type="button" class="btn btn-primary mb-2 mr-2 btn-rounded">Copy Link</button>
-                                                        </div>
-                                                      </div>
-
 
                 <!-- CONTENT AREA -->
+                <?php
+                 $link = "https://ersystem.000webhostapp.com"
+                 ?>
+     <!-- Modal -->
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" style="display: none;" aria-hidden="true">
+         <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalCenterTitle">Share Register Form</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                        <div class="modal-body">
+                             <div class= "row">
+                                     <div class="col">
+                                        <a href="https://www.facebook.com/sharer/sharer.php?u=<?=$link?>"target="_blank"> <img src="<?=base_url();?>/assets/img/social/facebook.png" alt="facebook" style="width:60px;"></a>
+                                    </div>
+                                    <div class="col">
+                                         <a href="#" target="_blank"> <img src="<?=base_url();?>/assets/img/social/instagram.png" alt="instragram" style="width:60px;"></a>
+                                    </div>
+                                    <div class="col">
+                                    <a href="https://www.linkedin.com/shareArticle?mini=true&url=<?=$link?>" target="_blank"> <img src="<?=base_url();?>/assets/img/social/linkedin.png" alt="linkedin" style="width:60px"></a>
+                                    </div>
+                                    <div class="col">
+                                     <a href="https://twitter.com/share?url=<?=$link?>&text=form&hashtags=events" target="_blank"> <img src="<?=base_url();?>/assets/img/social/twitter.png" alt="twiter" style="width:60px;"></a>
+                                    </div>
+                                    <div class="col">
+                                        <a href="https://api.whatsapp.com/send?text=event_form&url=<?=$link?>"target="_blank"> <img src="<?=base_url();?>/assets/img/social/whatsapp.png" alt="whatsapp" style="width:60px;"></a>
+                                    </div>
+                                    <div class="col">
+                                        <a href=""> <img src="<?=base_url();?>/assets/img/social/youtube.png" alt="youtube" style="width:60px;"></a>
+                                    </div>
+                                </div>
+                                    <hr>
+                            <div class ="form-row">
+                                  </div>
+                                    <div class="form-group col-sm-12">
+                                      <label for="copylink">copy Link</label>
+                                      <input type="text" name="link" value="<?=$link?>" class="form-control" id="link" readonly="readonly">
+                                     </div>
+                                     <div>
+                                      <button type="button"  onclick="copyLink()" class="btn btn-primary mb-2 mr-2 btn-rounded">Copy Link</button>
+                                      </div>
+                                     </div>
+                                    </div>
+                            </div>
+                            <script>
+                                function copyLink() {
+                                 var copyText = document.getElementById("link");
+                                copyText.select();
+                                copyText.setSelectionRange(0, 99999);
+                                document.execCommand("copy");
 
+                                }
+                            </script>
+                            
+                            
+                            
                     <!-- BEGIN GLOBAL MANDATORY STYLES -->
     <script src="assets/js/libs/jquery-3.1.1.min.js"></script>
     <script src="bootstrap/js/popper.min.js"></script>
@@ -155,6 +164,5 @@
 
     <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
     <script src="assets/js/pages/faq/faq.js"></script>
-
 
 
